0efa30b5e1a22277090ccd758296cead35aea8d5
duran-ni <duran_ni_7@hotmail.com>
2026-09-12 17:34:40 +0200

    feat: configure basic auth with cookie-based session management

---
+++ src/main/java/dev/duran/web_educAlba_backend/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package dev.duran.web_educAlba_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+// Esquema de autenticacion Basic Auth con sesion por cookies
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
