e0831076c3600110333a4fbc93525acd04f46e5c
duran-ni <duran_ni_7@hotmail.com>
2026-09-13 18:03:30 +0200

    refactor: annotate RoleService write method with @Transactional

--- src/main/java/dev/duran/web_educAlba_backend/user/RoleService.java
+++ src/main/java/dev/duran/web_educAlba_backend/user/RoleService.java
@@ -1,6 +1,7 @@
 package dev.duran.web_educAlba_backend.user;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RoleService {
@@ -11,8 +12,10 @@ public class RoleService {
         this.roleRepository = roleRepository;
     }
 
+    @Transactional
     public RoleEntity getOrCreateRole(String name) {
         return roleRepository.findByName(name)
             .orElseGet(() -> roleRepository.save(RoleEntity.builder().name(name).build()));
     }
+
 }
