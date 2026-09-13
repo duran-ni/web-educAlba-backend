caa22e59f2f5cb2861b7b28c585f66a362ed2328
duran-ni <duran_ni_7@hotmail.com>
2026-09-13 19:30:53 +0200

    feat: seed an initial admin user on application startup

--- src/main/java/dev/duran/web_educAlba_backend/user/DataSeeder.java
+++ src/main/java/dev/duran/web_educAlba_backend/user/DataSeeder.java
@@ -1,21 +1,48 @@
 package dev.duran.web_educAlba_backend.user;
 
+import java.util.Set;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import dev.duran.web_educAlba_backend.facade.encrypt.IEncryptFacade;
 
 // Se ejecuta automaticamente al arrancar la aplicacion, antes de aceptar peticiones
 @Component
 public class DataSeeder implements CommandLineRunner {
 
-    private final RoleService roleService;
+    private static final String INITIAL_ADMIN_EMAIL = "admin@educalba.com";
+    private static final String INITIAL_ADMIN_PASSWORD = "AdminEducAlba123";
 
-    public DataSeeder(RoleService roleService) {
+    private final RoleService roleService;
+    private final UserRepository userRepository;
+    private final IEncryptFacade encryptFacade;
+
+    public DataSeeder(RoleService roleService, UserRepository userRepository, IEncryptFacade encryptFacade) {
         this.roleService = roleService;
+        this.userRepository = userRepository;
+        this.encryptFacade = encryptFacade;
     }
 
     @Override
     public void run(String... args) {
-        roleService.getOrCreateRole(RoleNames.ADMIN);
+        RoleEntity adminRole = roleService.getOrCreateRole(RoleNames.ADMIN);
         roleService.getOrCreateRole(RoleNames.FAMILY);
+
+        seedInitialAdmin(adminRole);
+    }
+
+    private void seedInitialAdmin(RoleEntity adminRole) {
+        if (userRepository.existsByEmail(INITIAL_ADMIN_EMAIL)) {
+            return;
+        }
+
+        UserEntity admin = UserEntity.builder()
+            .email(INITIAL_ADMIN_EMAIL)
+            .password(encryptFacade.encode("bcrypt", INITIAL_ADMIN_PASSWORD))
+            .roles(Set.of(adminRole))
+            .build();
+
+        userRepository.save(admin);
     }
 }
