cb2d34dc010383e86e1e5bc8377602993832b71c
duran-ni <duran_ni_7@hotmail.com>
2026-09-24 09:21:11 +0200

    feat: add InterestSubmission entity and repository for the quick interest form

---
+++ src/main/java/dev/duran/web_educAlba_backend/academy/InterestSubmissionRepository.java
@@ -0,0 +1,6 @@
+package dev.duran.web_educAlba_backend.academy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestSubmissionRepository extends JpaRepository<InterestSubmission, Long> {
+}
